127e07cb15826e6e45ed3494708dacb708bd479c
Quint Guvernator <quint@guvernator.net>
2016-09-28 20:00:43 -0400

    test_vdot: (untested) BridgeMonitor whiteboard code from group meeting

--- threads/test_vdot.cc
+++ threads/test_vdot.cc
@@ -7,6 +7,42 @@
 #include "system.h"
 #include "synch.h"
 
+class BridgeMonitor
+{
+    static const int max_cars = 3;
+    static int cars = 0;
+    static int flow = 0;
+    static Lock lock;
+    static Condition depart;
+
+public:
+    void Arrive(int dir);
+    void Depart(int dir);
+}
+
+void BridgeMonitor::Arrive(int dir)
+{
+    bool wrong_way, full;
+    while ((wrong_way = (dir != flow)) ||
+           (full = (cars == max_cars))) {
+
+        if (full)
+            depart->Wait(lock);
+        else if (wrong_way && !cars)
+            direction = direction_desired;
+        else if (wrong_way)
+            depart->Wait(lock);
+    }
+}
+
+void BridgeMonitor::Depart(int _)
+{
+    lock->Acquire();
+    cars--;
+    depart->Broadcast(lock);
+    lock->Release(lock);
+}
+
 void TestVdot(void)
 {
 }
