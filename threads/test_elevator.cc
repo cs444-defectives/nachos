a45a3e6432fee092645dddf28c8fc60046a3c83f
Quint Guvernator <quint@guvernator.net>
2016-10-01 02:09:41 -0400

    test_elevator.cc: working demonstrable first pass

--- threads/test_elevator.cc
+++ threads/test_elevator.cc
@@ -7,6 +7,158 @@
 #include "system.h"
 #include "synch.h"
 
+#define NUM_FLOORS 4
+#define NUM_PASSENGERS 4
+
+static Lock *arrived_lock[NUM_FLOORS];
+static Condition *arrived[NUM_FLOORS];
+static int floor;
+
+static Lock *ready_lock[NUM_FLOORS];
+static Condition *ready[NUM_FLOORS];
+static int queued[NUM_FLOORS];
+
+/* for dynamically generating names */
+static char *arrived_lock_names[NUM_FLOORS], *arrived_cond_names[NUM_FLOORS],
+            *ready_lock_names[NUM_FLOORS], *ready_cond_names[NUM_FLOORS],
+            *thread_names[NUM_PASSENGERS];
+
+/*
+ * passes time by toggling interrupts, potentially a dangerous thing! RELEASE
+ * LOCKS BEFORE CALLING!
+ */
+static void tick(int n)
+{
+    ASSERT((IntOn == interrupt->SetLevel(IntOff)));
+    for (int i = 0; i < n; i++) {
+        interrupt->SetLevel(IntOff);
+        interrupt->SetLevel(IntOn);
+    }
+
+    /* in case no random seed is given */
+    currentThread->Yield();
+}
+
+static void elevate(int _)
+{
+    floor = 0;
+    int direction = 0;
+
+    while (true) {
+        printf("Elevator at floor %d\n", floor);
+
+        /* tell passengers they may leave or enter */
+        arrived_lock[floor]->Acquire();
+        arrived[floor]->Broadcast(arrived_lock[floor]);
+        arrived_lock[floor]->Release();
+
+        /* wait until everyone's in/out of the elevator */
+        ready_lock[floor]->Acquire();
+        while (queued[floor] != 0)
+            ready[floor]->Wait(ready_lock[floor]);
+        ready_lock[floor]->Release();
+
+        /* move to the next floor */
+        if (floor == 0 || floor == NUM_FLOORS - 1)
+            direction = !direction;
+        floor += direction ? 1 : -1;
+        tick(100);
+        currentThread->Yield();
+    }
+}
+
+static void waitForFloor(int n)
+{
+    ready_lock[n]->Acquire();
+    queued[n]++;
+    ready_lock[n]->Release();
+
+    arrived_lock[n]->Acquire();
+    while (floor != n)
+        arrived[n]->Wait(arrived_lock[n]);
+    arrived_lock[n]->Release();
+
+    ready_lock[n]->Acquire();
+    queued[n]--;
+    ready[n]->Signal(ready_lock[n]);
+    ready_lock[n]->Release();
+}
+
+static void getOnElevator(void)
+{
+    printf("%s gets on elevator at floor %d\n",
+           currentThread->getName(), floor);
+}
+
+static void getOffElevator(void)
+{
+    printf("%s gets off elevator at floor %d\n",
+           currentThread->getName(), floor);
+}
+
+/* I did not name this function */
+void ArrivingGoingFromTo(int atFloor, int toFloor)
+{
+    printf("%s waits at floor %d to go to floor %d\n",
+           currentThread->getName(), atFloor, toFloor);
+
+    waitForFloor(atFloor);
+    getOnElevator();
+
+    waitForFloor(toFloor);
+    getOffElevator();
+}
+
+static void passenge(int _)
+{
+    int from, to;
+    from = rand() % NUM_FLOORS;
+
+    /* make sure from and to aren't the same floor */
+    do
+        to = rand() % NUM_FLOORS;
+    while (to == from);
+
+    ArrivingGoingFromTo(from, to);
+}
+
 void TestElevator(void)
 {
+    ASSERT((NUM_FLOORS != 0));
+
+    char *m;
+
+    for (int i = 0; i < NUM_FLOORS; i++) {
+        m = arrived_cond_names[i];
+        m = new char[32];
+        snprintf(m, 32, "floor %d arrived", i);
+        arrived[i] = new Condition(m);
+
+        m = arrived_lock_names[i];
+        m = new char[32];
+        snprintf(m, 32, "floor %d arrived_lock", i);
+        arrived_lock[i] = new Lock(m);
+
+        m = ready_cond_names[i];
+        m = new char[32];
+        snprintf(m, 32, "floor %d ready", i);
+        ready[i] = new Condition(m);
+
+        m = ready_lock_names[i];
+        m = new char[32];
+        snprintf(m, 32, "floor %d ready_lock", i);
+        ready_lock[i] = new Lock(m);
+    }
+
+    Thread *elevator = new Thread("elevator");
+    elevator->Fork(elevate, 0);
+
+    Thread *passengers[NUM_PASSENGERS];
+    for (int i = 0; i < NUM_PASSENGERS; i++) {
+        m = thread_names[i];
+        m = new char[32];
+        snprintf(m, 32, "passenger %d", i);
+        passengers[i] = new Thread(m);
+        passengers[i]->Fork(passenge, 0);
+    }
 }
