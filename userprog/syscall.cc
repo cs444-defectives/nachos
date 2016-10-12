5e7a34e7900ee9792efe4e7dd7bba5f4fd1f8e23
kelvinabrokwa <kelvinabrokwa@gmail.com>
2016-10-12 17:27:32 -0400

    [2/userprog/create] get filesys in the mix

--- userprog/syscall.cc
+++ userprog/syscall.cc
@@ -3,7 +3,10 @@
  */
 #include "system.h"
 #include "syscall.h"
+#include "filesys.h"
 
 void Create(char *file_name)  {
     printf("creating file: %s\n", file_name);
+    FileSystem fs = new FileSystem(false);
+    fs.Create(file_name, 0);
 }
