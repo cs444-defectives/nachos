2d540e244d45953d8ba8b36a9287502f466c1149
Quint Guvernator <quint@guvernator.net>
2016-10-16 20:05:41 -0400

    new synchconsole.{cc,h}: synchronous console with blocking

---
+++ filesys/synchconsole.h
@@ -0,0 +1,49 @@
+#ifndef SYNCHDISK_H
+#define SYNCHDISK_H
+
+#include "synch.h"
+
+/*
+ * Routines to synchronously access the console. The console is an asynchronous
+ * device (requests return immediately, and an interrupt happens later on).
+ * This is a layer on top of the console providing a synchronous interface
+ * (requests wait until the request completes).
+ *
+ * We use a semaphore to synchronize the interrupt handlers with the pending
+ * requests. And, because the console can only handle one operation at a time,
+ * use a lock to enforce mutual exclusion.
+ */
+
+class SynchConsole {
+    public:
+    SynchConsole(void);
+    ~SynchConsole(void);
+
+    /*
+     * read/write from/to the console, returning only once the data is actually
+     * read or written
+     */
+    char ReadChar(void);
+    void ReadBytes(char *dest, int n);
+    void WriteChar(char c);
+    void WriteBytes(char *s, int n);
+
+    /*
+     * called (albeit indirectly) by the underlying console object when a key
+     * is pressed
+     */
+    void ReadAvailable();
+
+    /*
+     * called (albeit indirectly) by the underlying console object on character
+     * write to console
+     */
+    void WriteDone();
+
+    private:
+    Console *console;
+    Semaphore *reads, *writes;
+    Lock *lock;
+};
+
+#endif // SYNCHDISK_H
