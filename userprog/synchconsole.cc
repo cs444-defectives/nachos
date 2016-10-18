9a5b2e024a5368e7bad2016f3e3d340ea47aab7f
Quint Guvernator <quint@guvernator.net>
2016-10-18 12:49:47 -0400

    synchconsole moved whoops

---
+++ userprog/synchconsole.cc
@@ -0,0 +1,84 @@
+#include "synchconsole.h"
+#include <new>
+
+/*
+ * Since methods can't be used as callbacks, we need to wrap the read and write
+ * console callbacks.
+ */
+
+static void read_available(int console_addr)
+{
+    SynchConsole *console = (SynchConsole *) console_addr;
+    console->ReadAvailable();
+}
+
+static void write_done(int console_addr)
+{
+    SynchConsole *console = (SynchConsole *) console_addr;
+    console->WriteDone();
+}
+
+SynchConsole::SynchConsole()
+{
+    reads = new(std::nothrow) Semaphore("SynchConsole reads", 0);
+    writes = new(std::nothrow) Semaphore("SynchConsole writes", 0);
+    lock = new(std::nothrow) Lock("SynchConsole");
+    console = new(std::nothrow) Console(NULL, NULL, read_available, write_done, (int) this);
+}
+
+SynchConsole::~SynchConsole()
+{
+    delete console;
+    delete lock;
+    delete writes;
+    delete reads;
+}
+
+char SynchConsole::ReadChar(void)
+{
+    lock->Acquire();
+
+    /* wait until a character is available to read */
+    reads->P();
+    char c = console->GetChar();
+
+    lock->Release();
+    return c;
+}
+
+void SynchConsole::ReadBytes(char *dest, int n)
+{
+    lock->Acquire();
+    char c;
+    for (int i = 0; i < n; i++) {
+        reads->P();
+        c = console->GetChar();
+        ASSERT(c != EOF);
+        dest[i] = c;
+    }
+    lock->Release();
+}
+
+void SynchConsole::WriteChar(char c)
+{
+    lock->Acquire();
+    console->PutChar(c);
+
+    /* wait until the character is actually written to the console */
+    writes->P();
+
+    lock->Release();
+}
+
+void SynchConsole::WriteBytes(char *s, int n)
+{
+    lock->Acquire();
+    for (int i = 0; i < n; i++) {
+        console->PutChar(s[i]);
+        writes->P();
+    }
+    lock->Release();
+}
+
+void SynchConsole::ReadAvailable() { reads->V(); }
+void SynchConsole::WriteDone() { writes->V(); }
---
+++ userprog/synchconsole.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include "synch.h"
+#include "console.h"
+
+/*
+ * Routines to synchronously access the console. The console is an asynchronous
+ * device (requests return immediately, and an interrupt happens later on).
+ * This is a layer on top of the console providing a synchronous interface
+ * (requests wait until the request completes).
+ *
+ * We use a semaphore to synchronize the interrupt handlers with the pending
+ * requests. And, because the console can only handle one operation at a time,
+ * use a lock to enforce mutual exclusion.
+ */
+
+class SynchConsole {
+    public:
+    SynchConsole(void);
+    ~SynchConsole(void);
+
+    /*
+     * read/write from/to the console, returning only once the data is actually
+     * read or written
+     */
+    char ReadChar(void);
+    void ReadBytes(char *dest, int n);
+    void WriteChar(char c);
+    void WriteBytes(char *s, int n);
+
+    /*
+     * called (albeit indirectly) by the underlying console object when a key
+     * is pressed
+     */
+    void ReadAvailable();
+
+    /*
+     * called (albeit indirectly) by the underlying console object on character
+     * write to console
+     */
+    void WriteDone();
+
+    private:
+    Console *console;
+    Semaphore *reads, *writes;
+    Lock *lock;
+};
