6324cb117581bbed7aea8b7cd62d1acccb277593
kelvinabrokwa <kelvinabrokwa@gmail.com>
2016-10-27 16:51:04 -0400

    add spaceid field to thread object

--- threads/thread.h
+++ threads/thread.h
@@ -1,9 +1,9 @@
-// thread.h 
+// thread.h
 //	Data structures for managing threads.  A thread represents
 //	sequential execution of code within a program.
 //	So the state of a thread includes the program counter,
 //	the processor registers, and the execution stack.
-//	
+//
 // 	Note that because we allocate a fixed size stack for each
 //	thread, it is possible to overflow the stack -- for instance,
 //	by recursing to too deep a level.  The most common reason
@@ -17,12 +17,12 @@
 //		void foo() { int *buf = new int[1000]; ...}
 //
 //
-// 	Bad things happen if you overflow the stack, and in the worst 
+// 	Bad things happen if you overflow the stack, and in the worst
 //	case, the problem may not be caught explicitly.  Instead,
 //	the only symptom may be bizarre segmentation faults.  (Of course,
 //	other problems can cause seg faults, so that isn't a sure sign
 //	that your thread stacks are too small.)
-//	
+//
 //	One thing to try if you find yourself with seg faults is to
 //	increase the size of thread stack -- ThreadStackSize.
 //
@@ -31,7 +31,7 @@
 //	Only then can we do the fork: "t->fork(f, arg)".
 //
 // Copyright (c) 1992-1993 The Regents of the University of California.
-// All rights reserved.  See copyright.h for copyright notice and limitation 
+// All rights reserved.  See copyright.h for copyright notice and limitation
 // of liability and disclaimer of warranty provisions.
 
 #ifndef THREAD_H
@@ -43,14 +43,19 @@
 #ifdef USER_PROGRAM
 #include "machine.h"
 #include "addrspace.h"
+
+#ifdef CHANGED
+#include "syscall.h"
+#endif
+
 #endif
 
 #include "synch.h"
 
-// CPU register state to be saved on context switch.  
+// CPU register state to be saved on context switch.
 // The SPARC and MIPS only need 10 registers, but the Snake needs 18.
 // For simplicity, this is just the max over all architectures.
-#define MachineStateSize 18 
+#define MachineStateSize 18
 
 // Size of the thread's private execution stack.
 // WATCH OUT IF THIS ISN'T BIG ENOUGH!!!!!
@@ -60,7 +65,7 @@
 enum ThreadStatus { JUST_CREATED, RUNNING, READY, BLOCKED };
 
 // external function, dummy routine whose sole job is to call Thread::Print
-extern void ThreadPrint(int arg);	 
+extern void ThreadPrint(int arg);
 
 // The following class defines a "thread control block" -- which
 // represents a single thread of execution.
@@ -69,7 +74,7 @@ extern void ThreadPrint(int arg);
 //     an execution stack for activation records ("stackTop" and "stack")
 //     space to save CPU registers while not running ("machineState")
 //     a "status" (running/ready/blocked)
-//    
+//
 //  Some threads also belong to a user address space; threads
 //  that only run in the kernel have a NULL address space.
 
@@ -81,31 +86,35 @@ class Thread {
     int machineState[MachineStateSize];  // all registers except for stackTop
 
   public:
-    Thread(const char* debugName);		// initialize a Thread 
+    Thread(const char* debugName);		// initialize a Thread
     ~Thread(); 				// deallocate a Thread
 					// NOTE -- thread being deleted
-					// must not be running when delete 
+					// must not be running when delete
 					// is called
 
     // basic thread operations
 
     void Fork(VoidFunctionPtr func, int arg); 	// Make thread run (*func)(arg)
-    void Yield();  				// Relinquish the CPU if any 
+    void Yield();  				// Relinquish the CPU if any
 						// other thread is runnable
-    void Sleep();  				// Put the thread to sleep and 
+    void Sleep();  				// Put the thread to sleep and
 						// relinquish the processor
     void Finish();  				// The thread is done executing
-    
-    void CheckOverflow();   			// Check if thread has 
+
+    void CheckOverflow();   			// Check if thread has
 						// overflowed its stack
     void setStatus(ThreadStatus st) { status = st; }
     const char* getName() { return (name); }
     void Print() { printf("%s, ", name); }
 
+#ifdef CHANGED
+    SpaceId id;
+#endif
+
   private:
     // some of the private data for this class is listed above
-    
-    int* stack; 	 		// Bottom of the stack 
+
+    int* stack; 	 		// Bottom of the stack
 					// NULL if this is the main thread
 					// (If NULL, don't deallocate stack)
     ThreadStatus status;		// ready, running or blocked
@@ -116,8 +125,8 @@ class Thread {
 					// Used internally by Fork()
 
 #ifdef USER_PROGRAM
-// A thread running a user program actually has *two* sets of CPU registers -- 
-// one for its state while executing user code, one for its state 
+// A thread running a user program actually has *two* sets of CPU registers --
+// one for its state while executing user code, one for its state
 // while executing kernel code.
     int userRegisters[NumTotalRegs];	// user-level CPU register state
   public:
@@ -131,7 +140,7 @@ class Thread {
 // Magical machine-dependent routines, defined in switch.s
 
 extern "C" {
-// First frame on thread execution stack; 
+// First frame on thread execution stack;
 //   	enable interrupts
 //	call "func"
 //	(when func returns, if ever) call ThreadFinish()
