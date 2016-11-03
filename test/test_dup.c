a6bd092d62d4525ebdf0b8e957cdce2bcb18c443
Quint Guvernator <quint@guvernator.net>
2016-11-03 04:22:58 -0400

    add more dup tests, finds bug

--- test/test_dup.c
+++ test/test_dup.c
@@ -10,7 +10,26 @@ int main()
     Write("to original fid\n", 16, orig);
     Write("to duped fid\n", 13, duped);
     Close(orig);
-    Write("I can still write\n", 18, duped);
+    Write("original fid deleted, but I can still write\n", 44, duped);
     Close(duped);
+
+    Create("test_dup2.txt");
+    orig = Open("test_dup2.txt");
+    duped = Dup(orig);
+    Write("to original fid\n", 16, orig);
+    Write("to duped fid\n", 13, duped);
+    Close(duped);
+    Write("duped fid deleted, but I can still write\n", 41, orig);
+    Close(orig);
+
+    duped = Dup(ConsoleOutput);
+    Write("to original ConsoleOutput\n", 26, ConsoleOutput);
+    Write("to duped ConsoleOutput\n", 23, duped);
+    Close(ConsoleOutput);
+    Write("original fid deleted, but I can still write\n", 44, duped);
+
+    /* FIXME: this crashes the kernel */
+    /* Close(duped); */
+
     Halt();
 }
