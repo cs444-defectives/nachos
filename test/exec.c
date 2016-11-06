eb5715a3516b56cf7191a1406b48ccf40854fa86
Quint Guvernator <quint@guvernator.net>
2016-11-05 22:26:15 -0400

    test/exec.c: use new Exec signature

--- test/exec.c
+++ test/exec.c
@@ -4,11 +4,11 @@
 int main()
 {
   int s;
-  s = Exec("nonexistent file");
+  s = Exec("nonexistent file", (char **) 0);
   if (s == -1)
       print_string("exec on nonesxistent file returns -1\n");
   else
       print_string("FAIL: exec on nonesxistent file did not return -1\n");
-  Exec("test/fork");
+  Exec("test/fork", (char **) 0);
   print_string("if you see this, exec has returned\n");
 }
